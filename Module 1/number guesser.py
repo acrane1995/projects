5167a56b31aa152d935c745a50a8d4769f28309c
Anthony Crane <anthony.j.crane@gmail.com>
2020-01-17 18:51:34 -0500

    unfinished number guesser

--- Module 1/number guesser.py
+++ Module 1/number guesser.py
@@ -1,5 +1,4 @@
-count = 1
-x = 1000 * 1 // 2 ^ count
+x = 500
 print("Pick a number between 1 and 1000")
 print(x)
 print("Is this correct?")
@@ -11,12 +10,12 @@ else:
     print("Greater or less than?")
     result2 = input()
 
-count =+ 1
-
 if result2 == "greater":
-    print(x)
+    print(x * 1.5)
+    x = x * 1.5
 else:
     print(x // 2)
+    x = x // 2
 
 print("Is this correct?")
 result3 = input()
@@ -28,4 +27,21 @@ else:
     result4 = input()
 
 if result4 == "greater":
-    print("Yeet")
+    print(x * 1.5)
+else:
+    print(x // 2)
+
+print("Is this correct?")
+result5 = input()
+
+if result5 == "yes":
+    print("I GUESSED IT!")
+else:
+    print("Greater or less than?")
+    result6 = input()
+
+if result6 == "greater":
+    print(x)
+
+
+
