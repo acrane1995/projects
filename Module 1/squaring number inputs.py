7422186398f2465a7879926243e78e3d76fe549f
Anthony Crane <anthony.j.crane@gmail.com>
2020-01-21 13:46:51 -0500

    finished list of inputs squared

---
+++ Module 1/squaring number inputs.py
@@ -0,0 +1,17 @@
+print("Enter 5 numbers")
+num1 = int(input("First number"))
+num2 = int(input("Second number"))
+num3 = int(input("Third Number"))
+num4 = int(input("Fourth Number"))
+num5 = int(input("Fifth Number"))
+
+numlist = [num1, num2, num3, num4, num5]
+numlist2 = [num1**2, num2**2, num3**2, num4**2, num5**2]
+print(numlist)
+print(numlist2)
+
+#print(num1**2) #* num1)
+#print(num2**2) #* num2)
+#print(num3**2) #* num3)
+#print(num4**2) #* num4)
+#print(num5**2) #* num5)
